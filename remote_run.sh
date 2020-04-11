64ac021a36eb9d6678fdf8101cc7b15034e7bd2c
Sebastien Bourdeauducq <sb@m-labs.hk>
2020-04-11 21:12:41 +0800

    remote_run: remove unneeded ssh -n

--- remote_run.sh
+++ remote_run.sh
@@ -10,4 +10,4 @@ TARGET_FOLDER=/tmp/zynq-\$USER
 ssh $TARGET_HOST "mkdir -p $TARGET_FOLDER"
 rsync openocd/* $TARGET_HOST:$TARGET_FOLDER
 rsync target/armv7-none-eabihf/release/runtime $TARGET_HOST:$TARGET_FOLDER
-ssh -n $TARGET_HOST "cd $TARGET_FOLDER; openocd -f zc706.cfg -c 'load_image runtime; resume 0; exit'"
+ssh $TARGET_HOST "cd $TARGET_FOLDER; openocd -f zc706.cfg -c 'load_image runtime; resume 0; exit'"
