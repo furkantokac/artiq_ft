b7c5a56470845a3ec04e6b2af1821e3452014dad
Sebastien Bourdeauducq <sb@m-labs.hk>
2020-05-01 11:22:45 +0800

    remote_run: fixes

--- remote_run.sh
+++ remote_run.sh
@@ -21,10 +21,10 @@ target_folder=/tmp/zynq-\$USER
 ssh $target_host "mkdir -p $target_folder"
 rsync openocd/* $target_host:$target_folder
 if [ $impure -eq 1 ]; then
-    rsync src/target/armv7-none-eabihf/release/szl $target_host:$target_folder
+    rsync src/target/armv7-none-eabihf/release/szl $target_host:$target_folder/szl.elf
     rsync src/build/top.bit $target_host:$target_folder
 else
-    rsync result/szl $target_host:$target_folder
-    rsync result/top.bit $target_host:$target_folder
+    rsync -L result/szl.elf $target_host:$target_folder
+    rsync -L result/top.bit $target_host:$target_folder
 fi
-ssh $target_host "cd $target_folder; openocd -f zc706.cfg -c 'pld load 0 top.bit; load_image szl; resume 0; exit'"
+ssh $target_host "cd $target_folder; openocd -f zc706.cfg -c 'pld load 0 top.bit; load_image szl.elf; resume 0; exit'"
