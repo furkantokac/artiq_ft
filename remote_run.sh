8af8c894810b591aed539430279be4476d09cbe2
Sebastien Bourdeauducq <sb@m-labs.hk>
2020-07-04 23:33:10 +0800

    remote-run: use CRC for rsync of pure build products

--- remote_run.sh
+++ remote_run.sh
@@ -31,8 +31,8 @@ if [ $impure -eq 1 ]; then
     rsync $impure_dir/firmware/armv7-none-eabihf/release/szl $target_host:$target_folder/szl.elf
     rsync $impure_dir/gateware/top.bit $target_host:$target_folder
 else
-    rsync -L $pure_dir/szl.elf $target_host:$target_folder
-    rsync -L $pure_dir/top.bit $target_host:$target_folder
+    rsync -Lc $pure_dir/szl.elf $target_host:$target_folder
+    rsync -Lc $pure_dir/top.bit $target_host:$target_folder
 fi
 echo "Programming board..."
 ssh $target_host "cd $target_folder; openocd -f zc706.cfg -c 'pld load 0 top.bit; load_image szl.elf; resume 0; exit'"
