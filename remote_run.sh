d5df759341544233f01146f35704b8354261e431
Sebastien Bourdeauducq <sb@m-labs.hk>
2020-07-04 17:27:21 +0800

    remote-run: allow specifying artifact folder

--- remote_run.sh
+++ remote_run.sh
@@ -4,6 +4,8 @@ set -e
 
 target_host="rpi-4.m-labs.hk"
 impure=0
+pure_dir="result"
+impure_dir="build"
 
 while getopts "h:i" opt; do
     case "$opt" in
@@ -13,6 +15,9 @@ while getopts "h:i" opt; do
         ;;
     i)  impure=1
         ;;
+    d)  pure_dir=$OPTARG;
+        impure_dir=$OPTARG;
+        ;;
     esac
 done
 
@@ -21,10 +26,10 @@ target_folder=/tmp/zynq-\$USER
 ssh $target_host "mkdir -p $target_folder"
 rsync openocd/* $target_host:$target_folder
 if [ $impure -eq 1 ]; then
-    rsync build/firmware/armv7-none-eabihf/release/szl $target_host:$target_folder/szl.elf
-    rsync build/gateware/top.bit $target_host:$target_folder
+    rsync $impure_dir/firmware/armv7-none-eabihf/release/szl $target_host:$target_folder/szl.elf
+    rsync $impure_dir/gateware/top.bit $target_host:$target_folder
 else
-    rsync -L result/szl.elf $target_host:$target_folder
-    rsync -L result/top.bit $target_host:$target_folder
+    rsync -L $pure_dir/szl.elf $target_host:$target_folder
+    rsync -L $pure_dir/top.bit $target_host:$target_folder
 fi
 ssh $target_host "cd $target_folder; openocd -f zc706.cfg -c 'pld load 0 top.bit; load_image szl.elf; resume 0; exit'"
