caef2a9f84dccb366c0a11dc7cc4e09cb5939abc
pca006132 <john.lck40@gmail.com>
2020-07-16 16:24:27 +0800

    runtime/panic: prevent nested panic and added core ID in panic msg.

--- src/runtime/src/panic.rs
+++ src/runtime/src/panic.rs
@@ -1,8 +1,21 @@
 use libboard_zynq::{print, println};
+use libregister::RegisterR;
+use libcortex_a9::regs::MPIDR;
 use unwind::backtrace;
+
+static mut PANICKED: [bool; 2] = [false; 2];
 
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
+    let id = MPIDR.read().cpu_id() as usize;
+    print!("Core {} ", id);
+    unsafe {
+        if PANICKED[id] {
+            println!("nested panic!");
+            loop {}
+        }
+        PANICKED[id] = true;
+    }
     print!("panic at ");
     if let Some(location) = info.location() {
         print!("{}:{}:{}", location.file(), location.line(), location.column());
