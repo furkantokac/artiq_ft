2faf74f7083c4a8ba724b14c2d88e61d63519c2f
Sebastien Bourdeauducq <sb@m-labs.hk>
2020-08-25 10:49:00 +0800

    Revert "drop FSBL"
    Gitea issue #94
    
    This reverts commit 67ff3c36e242b5eb4fd4d4e6a59a266490e3d780.

--- default.nix
+++ default.nix
@@ -7,6 +7,7 @@ let
   rustPlatform = (import ./rustPlatform.nix { inherit pkgs; });
   artiqpkgs = import <artiq-fast/default.nix> { inherit pkgs; };
   vivado = import <artiq-fast/vivado.nix> { inherit pkgs; };
+  zc706-fsbl = import ./fsbl.nix { inherit pkgs; };
   mkbootimage = (import ./mkbootimage.nix { inherit pkgs; });
   build-zc706 = { variant }: let
     firmware = rustPlatform.buildRustPackage rec {
@@ -54,6 +55,7 @@ let
         echo file binary-dist $out/top.bit >> $out/nix-support/hydra-build-products
       '';
 
+    # SZL startup
     jtag = pkgs.runCommand "zc706-${variant}-jtag" {}
       ''
         mkdir $out
@@ -82,11 +84,36 @@ let
       mkbootimage boot.bif $out/boot.bin
       echo file binary-dist $out/boot.bin >> $out/nix-support/hydra-build-products
       '';
+
+    # FSBL startup
+    fsbl-sd = pkgs.runCommand "zc706-${variant}-fsbl-sd"
+      {
+        buildInputs = [ mkbootimage ];
+      }
+      ''
+      bifdir=`mktemp -d`
+      cd $bifdir
+      ln -s ${zc706-fsbl}/fsbl.elf fsbl.elf
+      ln -s ${gateware}/top.bit top.bit
+      ln -s ${firmware}/runtime.elf runtime.elf
+      cat > boot.bif << EOF
+      the_ROM_image:
+      {
+        [bootloader]fsbl.elf
+        top.bit
+        runtime.elf
+      }
+      EOF
+      mkdir $out $out/nix-support
+      mkbootimage boot.bif $out/boot.bin
+      echo file binary-dist $out/boot.bin >> $out/nix-support/hydra-build-products
+      '';
   in {
     "zc706-${variant}-firmware" = firmware;
     "zc706-${variant}-gateware" = gateware;
     "zc706-${variant}-jtag" = jtag;
     "zc706-${variant}-sd" = sd;
+    "zc706-${variant}-fsbl-sd" = fsbl-sd;
   };
 in
   (
@@ -95,5 +122,6 @@ in
     (build-zc706 { variant = "nist_qc2"; }) //
     (build-zc706 { variant = "acpki_simple"; }) //
     (build-zc706 { variant = "acpki_nist_clock"; }) //
-    (build-zc706 { variant = "acpki_nist_qc2"; })
+    (build-zc706 { variant = "acpki_nist_qc2"; }) //
+    { inherit zc706-fsbl; }
   )
---
+++ fsbl.nix
@@ -0,0 +1,37 @@
+{ pkgs, board ? "zc706" }:
+let
+  gnutoolchain = import ./gnutoolchain.nix { inherit pkgs; };
+in
+pkgs.stdenv.mkDerivation {
+  name = "${board}-fsbl";
+  src = pkgs.fetchFromGitHub {
+    owner = "Xilinx";
+    repo = "embeddedsw";
+    rev = "65c849ed46c88c67457e1fc742744f96db968ff1";
+    sha256 = "1rvl06ha40dzd6s9aa4sylmksh4xb9dqaxq462lffv1fdk342pda";
+  };
+  patches = [ ./fsbl.patch ];
+  nativeBuildInputs = [
+    pkgs.gnumake
+    gnutoolchain.binutils
+    gnutoolchain.gcc
+  ];
+  patchPhase =
+    ''
+    patch -p1 -i ${./fsbl.patch}
+    patchShebangs lib/sw_apps/zynq_fsbl/misc/copy_bsp.sh
+    echo 'SEARCH_DIR("${gnutoolchain.newlib}/arm-none-eabi/lib");' >> lib/sw_apps/zynq_fsbl/src/lscript.ld
+    '';
+  buildPhase =
+    ''
+    cd lib/sw_apps/zynq_fsbl/src
+    make BOARD=${board} "CFLAGS=-DFSBL_DEBUG_INFO -g"
+    '';
+  installPhase = 
+    ''
+    mkdir $out
+    cp fsbl.elf $out
+    '';
+  doCheck = false;
+  dontFixup = true;
+}
