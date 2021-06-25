4c2c23fcdd515879ce6248cd165fb728cda408b8
Sebastien Bourdeauducq <sb@m-labs.hk>
2021-06-25 16:03:26 +0800

    shell.nix: install pyftdi (for POR GPIO)

--- shell.nix
+++ shell.nix
@@ -22,7 +22,7 @@ in
       pkgs.openocd
       pkgs.openssh pkgs.rsync
 
-      (pkgs.python3.withPackages(ps: (with artiqpkgs; [ migen migen-axi misoc artiq artiq-netboot ps.jsonschema ])))
+      (pkgs.python3.withPackages(ps: (with artiqpkgs; [ migen migen-axi misoc artiq artiq-netboot ps.jsonschema ps.pyftdi ])))
       vivado
       artiqpkgs.binutils-arm
 
