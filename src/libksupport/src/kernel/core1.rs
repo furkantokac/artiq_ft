209eba33f0e2aa58e33fcb01ded9f87d22794167
mwojcik <mw@m-labs.hk>
2025-04-24 13:34:59 +0800

    ksupport: no rtio_init after forceful termination

--- src/libksupport/src/kernel/core1.rs
+++ src/libksupport/src/kernel/core1.rs
@@ -12,7 +12,7 @@ use libcortex_a9::{asm::{dsb, isb},
 use libsupport_zynq::ram;
 use log::{debug, error, info};
 
-use super::{api::resolve, dma, rpc::rpc_send_async, rtio, Message, CHANNEL_0TO1, CHANNEL_1TO0, CHANNEL_SEM, INIT_LOCK,
+use super::{api::resolve, dma, rpc::rpc_send_async, Message, CHANNEL_0TO1, CHANNEL_1TO0, CHANNEL_SEM, INIT_LOCK,
             KERNEL_CHANNEL_0TO1, KERNEL_CHANNEL_1TO0, KERNEL_IMAGE};
 use crate::{eh_artiq, get_async_errors};
 
@@ -141,8 +141,6 @@ pub extern "C" fn main_core1() {
         if !KERNEL_IMAGE.is_null() {
             // indicates forceful termination of previous kernel
             KERNEL_IMAGE = core::ptr::null();
-            debug!("rtio init");
-            rtio::init();
         }
         dma::init_dma_recorder();
     }
